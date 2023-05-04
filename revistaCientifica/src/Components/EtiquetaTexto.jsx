af7c96f1d46c2921b4e842b254f8b659a9d6b6bd
PabloGoProg <pgosorio13@gmail.com>
2023-05-04 12:00:21 -0500

    Componentes de icono y etiqueta

---
+++ revistaCientifica/src/Components/EtiquetaTexto.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function EtiquetaTexto({texto}) {
+    return (
+      <>
+        <section className="et">
+          <span className="text">{texto}</span>
+        </section>
+      </>
+    );
+}
+
--- revistaCientifica/src/main.jsx
+++ revistaCientifica/src/main.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Menu } from './Pages/Menu.jsx'
+import { Login } from './Pages/Login.jsx'
+import { Register } from './Pages/Register.jsx'
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import '../styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <h1>Hello world</h1>
+  <section className='mainContent'>
+    <HashRouter>
+      <Menu />
+
+      <Routes>
+      <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<p>Not found</p>} />
+      </Routes>
+    </HashRouter>
+  </section>
 )
