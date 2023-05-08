fb9426bbd7ee1b916eb978e05cbbc712f1a83145
Jimmygm16 <jimmya.giraldom@autonoma.edu.co>
2023-05-08 18:51:02 -0500

    first contribution

---
+++ revistaCientifica/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import '../Styles/Footer.css'
+
+export function Footer(){
+    return(
+        <footer className='generalFooter'>
+            <img className='logo_uam' src='src\Images\logo_uam.png'/>
+            <span className='footerText'>
+            Contacto: pjdevscontact@gmail.com
+            </span>
+        </footer>
+    );
+}
