7d91b026dde688a91a8982115aa4b16cbd684cc5
Jimmygm16 <jimmya.giraldom@autonoma.edu.co>
2023-05-08 22:15:22 -0500

    login first part

--- revistaCientifica/src/Pages/Login.jsx
+++ revistaCientifica/src/Pages/Login.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { ReactiveIcon } from "../Components/ReactiveIcon";
 import { EtiquetaTexto } from "../Components/EtiquetaTexto";
 import { TablaEdutor } from "../Components/TablaAutor";
 import { File } from "../Components/File";
 import '../Styles/Login.css'
+import {Button} from '../Components/Button'
 
 export function Login() {
-    return (
-        <section className="Login">
-            <EtiquetaTexto texto="Prueba de visualización" />
-
-            <ReactiveIcon
-              link="/register" 
-              srcImg="../Images/logoUAM.png" 
-            />
-
-            <TablaEdutor />
-            <File />
-        </section> 
+    return(
+        <section className="login_content">
+            <fragment className = 'login_image'>
+                <img src="src\Images\login_image.jpg" alt="" />
+            </fragment>
+            <section className="login_form">
+                <h2>
+                    Iniciar sesión
+                </h2>
+                <label>
+                    <span>Direccion de correo electronico</span>
+                    <input type="text" />
+                </label>
+                <label>
+                    <span>Contrasena</span>
+                    <input type="text"/>
+                </label>
+                <Button text={'Ingresar'}/>
+            </section>
+        </section>
     );
+
 }
